4a7896db9d3e7b03a4507d03f56940b18cd7e904
Adfmu1 <Adfmu3@gmail.com>
2026-04-02 14:13:40 +0200

    create server with a mux

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-
-
+	// create a multiplexer for a server
+	mux := http.NewServeMux()
+	// create server that uses created mux
+	serv := http.Server{
+		Addr:		":8080",
+		Handler:	mux,
+	}
+	// start the server
+	serv.ListenAndServe()
 }
