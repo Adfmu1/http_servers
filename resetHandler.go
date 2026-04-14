2776ee66c7bf0a305c4f4a375acd0f74564ede08
Adfmu1 <Adfmu3@gmail.com>
2026-04-14 22:49:35 +0200

    make reset endpoint remove all users from the database

--- resetHandler.go
+++ resetHandler.go
@@ -5,6 +5,20 @@ import (
 )
 
 func (cfg *apiConfig) handleResetEndpoint(rw http.ResponseWriter, req *http.Request) {
+	if cfg.Platform != "dev" {
+		const errMsg = "403 Forbidden"
+		respondWithError(rw, 403, errMsg)
+		return
+	}
+
+	err := apiConf.Database.DeleteUser(req.Context())
+
+	if err != nil {
+		const errMsg = "Couldnt delete users"
+		respondWithError(rw, 500, errMsg)
+		return
+	}
+
 	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader(200)
 	cfg.fileserverHits.Store(0)
