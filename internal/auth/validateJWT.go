d9b4135677033b2defc72c2673951a1199ac28ee
Adfmu1 <Adfmu3@gmail.com>
2026-04-21 13:16:04 +0200

    add validate JWT function

---
+++ internal/auth/validateJWT.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	sub, err := token.Claims.GetSubject()
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	id, err := uuid.Parse(sub)
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
+}
