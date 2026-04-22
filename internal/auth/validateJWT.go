05b1c98cf5babec7eb52bbe1bad9490a9fa0cb57
Adfmu1 <Adfmu3@gmail.com>
2026-04-22 10:30:08 +0200

    include missing checks in validateJWT function

--- internal/auth/validateJWT.go
+++ internal/auth/validateJWT.go
@@ -3,12 +3,13 @@ package auth
 import (
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"errors"
 )
 
 func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(tokenSecret), nil
-	})
+	},)
 
 	if err != nil {
 		return uuid.Nil, err
@@ -18,6 +19,14 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 
 	if err != nil {
 		return uuid.Nil, err
+	}
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string("chirpy-access") {
+		return uuid.Nil, errors.New("invalid issuer")
 	}
 
 	id, err := uuid.Parse(sub)
