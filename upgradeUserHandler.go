7722be3b4d247fc772407af4355ecda2c9ba593f
Adfmu1 <Adfmu3@gmail.com>
2026-04-25 16:28:26 +0200

    add checking API key in header to upgradeUserHandler

--- upgradeUserHandler.go
+++ upgradeUserHandler.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"github.com/Adfmu1/http_servers/internal/auth"
 	"github.com/google/uuid"
 	"net/http"
 	"encoding/json"
 )
 
 func upgradeUserHandler(rw http.ResponseWriter, req *http.Request) {
+	apiKey, err := auth.GetAPIKey(req.Header)
+
+	if err != nil {
+		const errMsg = "Wrong request header"
+		respondWithError(rw, 401, errMsg)
+		return
+	}
+
+	if apiKey != apiConf.PolkaKey {
+		const errMsg = "Wrong API key"
+		respondWithError(rw, 401, errMsg)
+		return
+	}
+
 	type parameters struct {
 		Event		string		`json:"event"`
 		Data	struct{
@@ -15,7 +30,7 @@ func upgradeUserHandler(rw http.ResponseWriter, req *http.Request) {
 	}
 	decoder := json.NewDecoder(req.Body)
 	params := parameters{}
-	err := decoder.Decode(&params)
+	err = decoder.Decode(&params)
 
 	if err != nil {
 		const errMsg = "Wrong request body"
