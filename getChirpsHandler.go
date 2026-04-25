1293f06a5fb1ebca8370e0d0756f83c6ba7de31b
Adfmu1 <Adfmu3@gmail.com>
2026-04-25 16:59:31 +0200

    add possibility to read a query from a request to get chirps of given user

--- getChirpsHandler.go
+++ getChirpsHandler.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"github.com/google/uuid"
 	"net/http"
 )
 
 func handleGetChirps(rw http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query().Get("author_id")
+
+	if query != "" {
+		usrId, err := uuid.Parse(query)
+
+		if err != nil {
+			const errMsg = "User doesnt exist"
+			respondWithError(rw, 404, errMsg)
+			return
+		}
+
+		chirps, err := apiConf.Database.GetChirpsByUserID(req.Context(), usrId)
+		
+		if err != nil {
+			const errMsg = "Something went wrong"
+			respondWithError(rw, 500, errMsg)
+			return
+		}
+
+		respondWithJson(rw, 200, chirps)
+		return
+	}
+
 	chirps, err := apiConf.Database.GetChirps(req.Context())
 
 	if err != nil {
